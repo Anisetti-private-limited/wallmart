5fe9b5bf8bed2d38a8fec8c75c8f03b62db3cf52
sri-anisetti <123366832+sri-anisetti@users.noreply.github.com>
2023-02-23 16:46:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-=-=<<<< HEAD
 hi i'm updating this file
-=======
 hi.. i'm  updating in the development 
->>>>>>> development
 Hello.. this file contain the develpoment script
  hhi i am updating this file  
+i'm from uat branch 
